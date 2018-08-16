81749608b39d98fe16a7a29cdb3e95d75997b999
jamie <jamie.coales@education.gov.uk>
2018-08-16 15:41:31 +0100

    Add new report stub and type

--- src/ESFA.DC.ILR1819.ReportService.Interface/Reports/ReportType.cs
+++ src/ESFA.DC.ILR1819.ReportService.Interface/Reports/ReportType.cs
@@ -7,6 +7,7 @@
         MainOccupancy,
         MathsAndEnglish,
         SummaryOfFunding1619,
-        ValidationErrors
+        ValidationErrors,
+        SummaryOfFM35Funding
     }
 }
---
+++ src/ESFA.DC.ILR1819.ReportService.Service/Reports/SummaryOfFM35FundingReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR1819.ReportService.Interface.Reports;
+using ESFA.DC.ILR1819.ReportService.Model.Report;
+using ESFA.DC.JobContext.Interface;
+
+namespace ESFA.DC.ILR1819.ReportService.Service.Reports
+{
+    public class SummaryOfFM35FundingReport : IReport
+    {
+        public ReportType ReportType => ReportType.SummaryOfFM35Funding;
+
+        public Task GenerateReport(IJobContextMessage jobContextMessage, ZipArchive archive, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetReportFilename()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
