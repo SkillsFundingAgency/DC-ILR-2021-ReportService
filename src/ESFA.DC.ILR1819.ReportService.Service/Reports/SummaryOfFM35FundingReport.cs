f8478d6d0417f3b7296a9694f51097d73015b454
jamie <jamie.coales@education.gov.uk>
2018-08-17 09:40:58 +0100

    Inherit from base class

--- src/ESFA.DC.ILR1819.ReportService.Service/Reports/SummaryOfFM35FundingReport.cs
+++ src/ESFA.DC.ILR1819.ReportService.Service/Reports/SummaryOfFM35FundingReport.cs
@@ -5,22 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ESFA.DC.DateTimeProvider.Interface;
 using ESFA.DC.ILR1819.ReportService.Interface.Reports;
 using ESFA.DC.ILR1819.ReportService.Model.Report;
 using ESFA.DC.JobContext.Interface;
 
 namespace ESFA.DC.ILR1819.ReportService.Service.Reports
 {
-    public class SummaryOfFM35FundingReport : IReport
+    public class SummaryOfFM35FundingReport : AbstractReportBuilder, IReport
     {
+        public SummaryOfFM35FundingReport(IDateTimeProvider dateTimeProvider)
+            : base(dateTimeProvider)
+        {
+            ReportName = "foo";
+        }
+
         public ReportType ReportType => ReportType.SummaryOfFM35Funding;
 
         public Task GenerateReport(IJobContextMessage jobContextMessage, ZipArchive archive, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetReportFilename()
         {
             throw new NotImplementedException();
         }
