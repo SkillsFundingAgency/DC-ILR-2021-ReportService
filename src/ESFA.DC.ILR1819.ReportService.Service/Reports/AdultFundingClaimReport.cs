e400e9a39a09739400f0dfaec068af3a8a9ac021
Hari Dupati <dupatihari@gmail.com>
2018-11-07 13:39:05 +0000

    62801- Added Excel template and tests to see if it works with the object model properly. Added Model and Report.

---
+++ src/ESFA.DC.ILR1819.ReportService.Service/Reports/AdultFundingClaimReport.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Aspose.Cells;
+using CsvHelper;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.EAS1819.EF;
+using ESFA.DC.ILR.FundingService.ALB.FundingOutput.Model.Output;
+using ESFA.DC.ILR.FundingService.FM25.Model.Output;
+using ESFA.DC.ILR.FundingService.FM35.FundingOutput.Model.Output;
+using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
+using ESFA.DC.ILR.FundingService.FM81.FundingOutput.Model.Output;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR1819.ReportService.Interface.Builders;
+using ESFA.DC.ILR1819.ReportService.Interface.Configuration;
+using ESFA.DC.ILR1819.ReportService.Interface.Reports;
+using ESFA.DC.ILR1819.ReportService.Interface.Service;
+using ESFA.DC.ILR1819.ReportService.Model.Generation;
+using ESFA.DC.ILR1819.ReportService.Model.ReportModels;
+using ESFA.DC.ILR1819.ReportService.Model.Styling;
+using ESFA.DC.ILR1819.ReportService.Service.Mapper;
+using ESFA.DC.IO.Interfaces;
+using ESFA.DC.JobContext.Interface;
+using ESFA.DC.JobContextManager.Model.Interface;
+using ESFA.DC.Logging.Interfaces;
+
+namespace ESFA.DC.ILR1819.ReportService.Service.Reports
+{
+    public sealed class AdultFundingClaimReport : AbstractReportBuilder, IReport
+    {
+        private const string ProviderNameCellName = "D5";
+        private const string UkprnCellName = "D6";
+        private const string IlrFileCellName = "D7";
+        private const string YearCellName = "G7";
+        private const string OtherLearningProgrammeFunding6MonthsCellName = "F12";
+        private const string OtherLearningProgrammeFunding12MonthsCellName = "G12";
+        private const string OtherLearningLearningSupport6MonthsCellName = "F13";
+        private const string OtherLearningLearningSupport12MonthsCellName = "G13";
+        private const string Traineeships1924ProgrammeFunding6MonthsCellName = "F14";
+        private const string Traineeships1924ProgrammeFunding12MonthsCellName = "G14";
+        private const string Traineeships1924LearningSupport6MonthsCellName = "F15";
+        private const string Traineeships1924LearningSupport12MonthsCellName = "G15";
+        private const string Traineeships1924LearnerSupport6MonthsCellName = "F16";
+        private const string Traineeships1924LearnerSupport12MonthsCellName = "G16";
+        private const string LoansBursaryFunding6MonthsCellName = "F21";
+        private const string LoansBursaryFunding12MonthsCellName = "G21";
+        private const string LoansAreaCosts6MonthsCellName = "F22";
+        private const string LoansAreaCosts12MonthsCellName = "G22";
+        private const string LoansExcessSupport6MonthsCellName = "F23";
+        private const string LoansExcessSupport12MonthsCellName = "G23";
+        private const string ComponentSetVersionCellName = "D36";
+        private const string ApplicationVersionCellName = "D37";
+        private const string FilePreparationDateCellName = "D38";
+        private const string LarsDataCellName = "F36";
+        private const string OrganisationDataCellName = "F37";
+        private const string PostcodeDataCellName = "F38";
+        private const string LargeEmployerDataCellName = "F39";
+        private const string ReportGeneratedAtCellName = "D40";
+
+        private readonly FundingSummaryMapper _fundingSummaryMapper;
+        private readonly ModelProperty[] _cachedModelProperties;
+        private readonly List<FundingSummaryModel> fundingSummaryModels;
+        private readonly ILogger _logger;
+        private readonly IStreamableKeyValuePersistenceService _storage;
+        private readonly IIlrProviderService _ilrProviderService;
+        private readonly IOrgProviderService _orgProviderService;
+        private readonly IAllbProviderService _allbProviderService;
+        private readonly IFM35ProviderService _fm35ProviderService;
+        private readonly IPeriodProviderService _periodProviderService;
+        private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly ILarsProviderService _larsProviderService;
+        private readonly IEasProviderService _easProviderService;
+        private readonly IPostcodeProviderService _postcodeProviderService;
+        private readonly ILargeEmployerProviderService _largeEmployerProviderService;
+
+        private readonly ITotalBuilder _totalBuilder;
+        private readonly IVersionInfo _versionInfo;
+        private readonly IExcelStyleProvider _excelStyleProvider;
+        private readonly IEasBuilder _easBuilder;
+        private readonly IAllbBuilder _allbBuilder;
+        private readonly IFm25Builder _fm25Builder;
+        private readonly IFm35Builder _fm35Builder;
+
+        public AdultFundingClaimReport(
+            ILogger logger,
+            IStreamableKeyValuePersistenceService storage,
+            IIlrProviderService ilrProviderService,
+            IOrgProviderService orgProviderService,
+            IAllbProviderService allbProviderService,
+            IFM35ProviderService fm35ProviderService,
+            IPeriodProviderService periodProviderService,
+            IDateTimeProvider dateTimeProvider,
+            IValueProvider valueProvider,
+            ILarsProviderService larsProviderService,
+            IEasProviderService easProviderService,
+            IPostcodeProviderService postcodeProviderService,
+            ILargeEmployerProviderService largeEmployerProviderService,
+            IAllbBuilder allbBuilder,
+            IFm35Builder fm35Builder,
+            ITotalBuilder totalBuilder,
+            IVersionInfo versionInfo,
+            IExcelStyleProvider excelStyleProvider,
+            ITopicAndTaskSectionOptions topicAndTaskSectionOptions,
+            IEasBuilder easBuilder)
+            : base(dateTimeProvider, valueProvider)
+        {
+            _logger = logger;
+            _storage = storage;
+            _ilrProviderService = ilrProviderService;
+            _orgProviderService = orgProviderService;
+            _allbProviderService = allbProviderService;
+            _fm35ProviderService = fm35ProviderService;
+            _periodProviderService = periodProviderService;
+            _larsProviderService = larsProviderService;
+            _easProviderService = easProviderService;
+            _postcodeProviderService = postcodeProviderService;
+            _largeEmployerProviderService = largeEmployerProviderService;
+            _allbBuilder = allbBuilder;
+            _fm35Builder = fm35Builder;
+            _totalBuilder = totalBuilder;
+            _versionInfo = versionInfo;
+            _excelStyleProvider = excelStyleProvider;
+            _dateTimeProvider = dateTimeProvider;
+            _easBuilder = easBuilder;
+
+            ReportFileName = "Adult Funding Claim Report";
+            ReportTaskName = topicAndTaskSectionOptions.TopicReports_TaskGenerateFundingSummaryReport;
+
+            fundingSummaryModels = new List<FundingSummaryModel>();
+            _fundingSummaryMapper = new FundingSummaryMapper();
+            _cachedModelProperties = _fundingSummaryMapper.MemberMaps.OrderBy(x => x.Data.Index).Select(x => new ModelProperty(x.Data.Names.Names.ToArray(), (PropertyInfo)x.Data.Member)).ToArray();
+        }
+
+        public async Task GenerateReport(IJobContextMessage jobContextMessage, ZipArchive archive, bool isFis, CancellationToken cancellationToken)
+        {
+            //Task<IMessage> ilrFileTask = _ilrProviderService.GetIlrFile(jobContextMessage, cancellationToken);
+            //Task<ALBGlobal> albDataTask = _allbProviderService.GetAllbData(jobContextMessage, cancellationToken);
+            //Task<FM35Global> fm35Task = _fm35ProviderService.GetFM35Data(jobContextMessage, cancellationToken);
+            //Task<string> providerNameTask = _orgProviderService.GetProviderName(jobContextMessage, cancellationToken);
+            //Task<int> periodTask = _periodProviderService.GetPeriod(jobContextMessage, cancellationToken);
+
+            //Task<List<EasSubmissionValues>> easSubmissionsValuesTask = new Task<List<EasSubmissionValues>>(null);
+            //if (!isFis)
+            //{
+            //    easSubmissionsValuesTask = _easProviderService.GetEasSubmissionValuesAsync(jobContextMessage, cancellationToken);
+            //}
+
+            //await Task.WhenAll(ilrFileTask, albDataTask, fm35Task, providerNameTask, periodTask, easSubmissionsValuesTask);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            var jobId = jobContextMessage.JobId;
+            var ukPrn = jobContextMessage.KeyValuePairs[JobContextMessageKey.UkPrn].ToString();
+            var externalFileName = GetExternalFilename(ukPrn, jobId, jobContextMessage.SubmissionDateTimeUtc);
+            var fileName = GetFilename(ukPrn, jobId, jobContextMessage.SubmissionDateTimeUtc);
+
+            var adultFundingClaimModel = new AdultFundingClaimModel()
+            {
+                ProviderName = "Milton Keynes Council",
+                Ukprn = 10004376,
+                IlrFile = "ILR-A-10004376-1819-20181119-151322-01.xml",
+                Year = "2018/19",
+
+                OtherLearningProgrammeFunding6Months = 1006,
+                OtherLearningProgrammeFunding12Months = 1012,
+                OtherLearningLearningSupport6Months = 2006,
+                OtherLearningLearningSupport12Months = 2012,
+                Traineeships1924ProgrammeFunding6Months = 3006,
+                Traineeships1924ProgrammeFunding12Months = 3012,
+                Traineeships1924LearningSupport6Months = 4006,
+                Traineeships1924LearningSupport12Months = 4012,
+                Traineeships1924LearnerSupport6Months = 5006,
+                Traineeships1924LearnerSupport12Months = 5012,
+                LoansBursaryFunding6Months = 6006,
+                LoansBursaryFunding12Months = 6012,
+                LoansAreaCosts6Months = 7006,
+                LoansAreaCosts12Months = 7012,
+                LoansExcessSupport6Months = 8006,
+                LoansExcessSupport12Months = 8012,
+
+                ComponentSetVersion = "12",
+                ApplicationVersion = "1.2.3.4",
+                FilePreparationDate = "07/11/2018",
+                LarsData = "Version 1.1.1: " + _dateTimeProvider.GetNowUtc().ToString("dd MMM yyyy HH:mm:ss"),
+                OrganisationData = "Version 2.2.2: " + _dateTimeProvider.GetNowUtc().ToString("dd MMM yyyy HH:mm:ss"),
+                PostcodeData = "Version 3.3.3: " + _dateTimeProvider.GetNowUtc().ToString("dd MMM yyyy HH:mm:ss"),
+                LargeEmployerData = "Version 4.4.4: " + _dateTimeProvider.GetNowUtc().ToString("dd MMM yyyy HH:mm:ss"),
+                ReportGeneratedAt = _dateTimeProvider.GetNowUtc().ToString("HH:mm:ss tt") + " on " + _dateTimeProvider.GetNowUtc().ToString("dd/MM/yyyy")
+            };
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                _storage.GetAsync("AdultFundingClaimReportTemplate", memoryStream, cancellationToken).GetAwaiter().GetResult();
+
+                memoryStream.Position = 0;
+                Workbook workbook = new Workbook(memoryStream);
+                PopulateWorkbook(workbook, adultFundingClaimModel);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    workbook.Save(ms, SaveFormat.Xlsx);
+                    await _storage.SaveAsync($"{externalFileName}.xlsx", ms, cancellationToken);
+                    await WriteZipEntry(archive, $"{fileName}.xlsx", ms, cancellationToken);
+                }
+            }
+        }
+
+        private void PopulateWorkbook(Workbook workbook, AdultFundingClaimModel adultFundingClaimModel)
+        {
+            Worksheet worksheet = workbook.Worksheets[0];
+            Cells cells = worksheet.Cells;
+            cells[ProviderNameCellName].PutValue(adultFundingClaimModel.ProviderName);
+            cells[UkprnCellName].PutValue(adultFundingClaimModel.Ukprn);
+            cells[IlrFileCellName].PutValue(adultFundingClaimModel.IlrFile);
+            cells[YearCellName].PutValue(adultFundingClaimModel.Year);
+
+            cells[OtherLearningProgrammeFunding6MonthsCellName]
+                .PutValue(adultFundingClaimModel.OtherLearningProgrammeFunding6Months);
+            cells[OtherLearningProgrammeFunding12MonthsCellName]
+                .PutValue(adultFundingClaimModel.OtherLearningProgrammeFunding12Months);
+            cells[OtherLearningLearningSupport6MonthsCellName]
+                .PutValue(adultFundingClaimModel.OtherLearningLearningSupport6Months);
+            cells[OtherLearningLearningSupport12MonthsCellName]
+                .PutValue(adultFundingClaimModel.OtherLearningLearningSupport12Months);
+            cells[Traineeships1924ProgrammeFunding6MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924ProgrammeFunding6Months);
+            cells[Traineeships1924ProgrammeFunding12MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924ProgrammeFunding12Months);
+            cells[Traineeships1924LearningSupport6MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924LearningSupport6Months);
+            cells[Traineeships1924LearningSupport12MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924LearningSupport12Months);
+            cells[Traineeships1924LearnerSupport6MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924LearnerSupport6Months);
+            cells[Traineeships1924LearnerSupport12MonthsCellName]
+                .PutValue(adultFundingClaimModel.Traineeships1924LearnerSupport12Months);
+            cells[LoansBursaryFunding6MonthsCellName].PutValue(adultFundingClaimModel.LoansBursaryFunding6Months);
+            cells[LoansBursaryFunding12MonthsCellName].PutValue(adultFundingClaimModel.LoansBursaryFunding12Months);
+            cells[LoansAreaCosts6MonthsCellName].PutValue(adultFundingClaimModel.LoansAreaCosts6Months);
+            cells[LoansAreaCosts12MonthsCellName].PutValue(adultFundingClaimModel.LoansAreaCosts12Months);
+            cells[LoansExcessSupport6MonthsCellName].PutValue(adultFundingClaimModel.LoansExcessSupport6Months);
+            cells[LoansExcessSupport12MonthsCellName].PutValue(adultFundingClaimModel.LoansExcessSupport12Months);
+
+            cells[ComponentSetVersionCellName].PutValue(adultFundingClaimModel.ComponentSetVersion);
+            cells[ApplicationVersionCellName].PutValue(adultFundingClaimModel.ApplicationVersion);
+            cells[FilePreparationDateCellName].PutValue(adultFundingClaimModel.FilePreparationDate);
+            cells[LarsDataCellName].PutValue(adultFundingClaimModel.LarsData);
+            cells[OrganisationDataCellName].PutValue(adultFundingClaimModel.OrganisationData);
+            cells[PostcodeDataCellName].PutValue(adultFundingClaimModel.PostcodeData);
+            cells[LargeEmployerDataCellName].PutValue(adultFundingClaimModel.LargeEmployerData);
+            cells[ReportGeneratedAtCellName].PutValue(adultFundingClaimModel.ReportGeneratedAt);
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR1819.ReportService.Tests/Reports/TestAdultFundingClaimReport.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.EAS1819.EF;
+using ESFA.DC.EAS1819.EF.Interface;
+using ESFA.DC.ILR.FundingService.FM35.FundingOutput.Model.Output;
+using ESFA.DC.ILR1819.ReportService.Interface.Builders;
+using ESFA.DC.ILR1819.ReportService.Interface.Configuration;
+using ESFA.DC.ILR1819.ReportService.Interface.Service;
+using ESFA.DC.ILR1819.ReportService.Model.Configuration;
+using ESFA.DC.ILR1819.ReportService.Service;
+using ESFA.DC.ILR1819.ReportService.Service.Builders;
+using ESFA.DC.ILR1819.ReportService.Service.Reports;
+using ESFA.DC.ILR1819.ReportService.Service.Service;
+using ESFA.DC.ILR1819.ReportService.Stateless.Configuration;
+using ESFA.DC.ILR1819.ReportService.Tests.AutoFac;
+using ESFA.DC.IO.Interfaces;
+using ESFA.DC.JobContext.Interface;
+using ESFA.DC.JobContextManager.Model;
+using ESFA.DC.JobContextManager.Model.Interface;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.Serialization.Interfaces;
+using ESFA.DC.Serialization.Json;
+using ESFA.DC.Serialization.Xml;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR1819.ReportService.Tests.Reports
+{
+    public sealed class TestAdultFundingClaimReport
+    {
+        [Fact]
+        public async Task TestAdultFundingClaimReportGeneration()
+        {
+            string csv = string.Empty;
+            byte[] xlsx = null;
+            DateTime dateTime = DateTime.UtcNow;
+            string filename = $"10033670_1_Adult Funding Claim Report {dateTime:yyyyMMdd-HHmmss}";
+
+            Mock<ILogger> logger = new Mock<ILogger>();
+            Mock<IStreamableKeyValuePersistenceService> storage = new Mock<IStreamableKeyValuePersistenceService>();
+            Mock<IKeyValuePersistenceService> redis = new Mock<IKeyValuePersistenceService>();
+            IJsonSerializationService jsonSerializationService = new JsonSerializationService();
+            IXmlSerializationService xmlSerializationService = new XmlSerializationService();
+            IIlrProviderService ilrProviderService = new IlrProviderService(logger.Object, storage.Object, xmlSerializationService);
+            Mock<IOrgProviderService> orgProviderService = new Mock<IOrgProviderService>();
+            Mock<ILarsProviderService> larsProviderService = new Mock<ILarsProviderService>();
+
+            EasConfiguration easConfiguration = new EasConfiguration() { EasConnectionString = new TestConfigurationHelper().GetSectionValues<EasConfiguration>("EasSection").EasConnectionString };
+            IEasProviderService easProviderService = new EasProviderService(logger.Object, easConfiguration);
+
+            Mock <IPostcodeProviderService> postcodeProverServiceMock = new Mock<IPostcodeProviderService>();
+            Mock<ILargeEmployerProviderService> largeEmployerProviderService = new Mock<ILargeEmployerProviderService>();
+            IAllbProviderService allbProviderService = new AllbProviderService(logger.Object, redis.Object, storage.Object, jsonSerializationService);
+            IFM35ProviderService fm35ProviderService = new FM35ProviderService(logger.Object, redis.Object, storage.Object, jsonSerializationService);
+            IValidLearnersService validLearnersService = new ValidLearnersService(logger.Object, redis.Object, storage.Object, jsonSerializationService);
+            IStringUtilitiesService stringUtilitiesService = new StringUtilitiesService();
+            Mock<IPeriodProviderService> periodProviderService = new Mock<IPeriodProviderService>();
+            Mock<IDateTimeProvider> dateTimeProviderMock = new Mock<IDateTimeProvider>();
+            IVersionInfo versionInfo = new VersionInfo { ServiceReleaseVersion = "1.2.3.4.5" };
+            ITotalBuilder totalBuilder = new TotalBuilder();
+            IFm35Builder fm35Builder = new Fm35Builder(totalBuilder, new CacheProviderService<LearningDelivery[]>());
+            IAllbBuilder allbBuilder = new AllbBuilder(ilrProviderService, validLearnersService, allbProviderService, periodProviderService.Object, totalBuilder, stringUtilitiesService, logger.Object);
+            IExcelStyleProvider excelStyleProvider = new ExcelStyleProvider();
+
+            IEasBuilder easBuilder = new EasBuilder(easProviderService);
+
+            storage.Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<Stream>(), It.IsAny<CancellationToken>())).Callback<string, Stream, CancellationToken>((st, sr, ct) => File.OpenRead("AdultFundingClaimReportTemplate.xlsx").CopyTo(sr)).Returns(Task.CompletedTask);
+            storage.Setup(x => x.SaveAsync($"{filename}.csv", It.IsAny<string>(), It.IsAny<CancellationToken>())).Callback<string, string, CancellationToken>((key, value, ct) => csv = value).Returns(Task.CompletedTask);
+            storage.Setup(x => x.SaveAsync($"{filename}.xlsx", It.IsAny<Stream>(), It.IsAny<CancellationToken>())).Callback<string, Stream, CancellationToken>(
+                    (key, value, ct) =>
+                    {
+                        value.Seek(0, SeekOrigin.Begin);
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            value.CopyTo(ms);
+                            xlsx = ms.ToArray();
+                        }
+                    })
+                .Returns(Task.CompletedTask);
+            redis.Setup(x => x.GetAsync("FundingAlbOutput", It.IsAny<CancellationToken>())).ReturnsAsync(File.ReadAllText("ALBOutput1000.json"));
+            redis.Setup(x => x.GetAsync("ValidLearners", It.IsAny<CancellationToken>())).ReturnsAsync(jsonSerializationService.Serialize(
+                new List<string>
+                {
+                    "3Addl103",
+                    "4Addl103"
+                }));
+            redis.Setup(x => x.GetAsync("FundingFm35Output", It.IsAny<CancellationToken>())).ReturnsAsync(File.ReadAllText("Fm35.json"));
+            redis.Setup(x => x.GetAsync("FundingFm25Output", It.IsAny<CancellationToken>())).ReturnsAsync(File.ReadAllText("Fm25.json"));
+            periodProviderService.Setup(x => x.GetPeriod(It.IsAny<IJobContextMessage>(), It.IsAny<CancellationToken>())).ReturnsAsync(12);
+            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(dateTime);
+            dateTimeProviderMock.Setup(x => x.ConvertUtcToUk(It.IsAny<DateTime>())).Returns(dateTime);
+            largeEmployerProviderService.Setup(x => x.GetVersionAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync("NA");
+            postcodeProverServiceMock.Setup(x => x.GetVersionAsync(It.IsAny<CancellationToken>())).ReturnsAsync("NA");
+            orgProviderService
+                .Setup(x => x.GetProviderName(It.IsAny<IJobContextMessage>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync("Test Provider");
+
+            ITopicAndTaskSectionOptions topicsAndTasks = TestConfigurationHelper.GetTopicsAndTasks();
+            IValueProvider valueProvider = new ValueProvider();
+
+            AdultFundingClaimReport adultFundingClaimReport = new AdultFundingClaimReport(
+                logger.Object,
+                storage.Object,
+                ilrProviderService,
+                orgProviderService.Object,
+                allbProviderService,
+                fm35ProviderService,
+                periodProviderService.Object,
+                dateTimeProviderMock.Object,
+                valueProvider,
+                larsProviderService.Object,
+                easProviderService,
+                postcodeProverServiceMock.Object,
+                largeEmployerProviderService.Object,
+                allbBuilder,
+                fm35Builder,
+                totalBuilder,
+                versionInfo,
+                excelStyleProvider,
+                topicsAndTasks,
+                easBuilder);
+            IJobContextMessage jobContextMessage = new JobContextMessage(1, new ITopicItem[0], 0, DateTime.UtcNow);
+
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.UkPrn] = "10033670";
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.Filename] = "ILR-10033670-1819-20180704-120055-03";
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.FundingAlbOutput] = "FundingAlbOutput";
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.ValidLearnRefNumbers] = "ValidLearners";
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.FundingFm35Output] = "FundingFm35Output";
+            jobContextMessage.KeyValuePairs[JobContextMessageKey.FundingFm25Output] = "FundingFm25Output";
+
+            await adultFundingClaimReport.GenerateReport(jobContextMessage, null, false, CancellationToken.None);
+
+            xlsx.Should().NotBeNullOrEmpty();
+
+            File.WriteAllBytes($"{filename}.xlsx", xlsx);
+        }
+    }
+}
