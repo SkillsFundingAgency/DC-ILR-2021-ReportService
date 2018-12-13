f573d1c03198676f6c68d27a528985bc86acc03b
John Youren <john.youren@gmail.com>
2018-12-13 15:35:56 +0000

    Updated org data source to EF core

--- src/ESFA.DC.ILR1819.ReportService.Service/Service/OrgProviderService.cs
+++ src/ESFA.DC.ILR1819.ReportService.Service/Service/OrgProviderService.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ESFA.DC.Data.Organisatons.Model;
 using ESFA.DC.ILR1819.ReportService.Interface.Context;
 using ESFA.DC.ILR1819.ReportService.Interface.Service;
 using ESFA.DC.ILR1819.ReportService.Model.Configuration;
 using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.ReferenceData.Organisations.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace ESFA.DC.ILR1819.ReportService.Service.Service
 {
@@ -50,9 +50,13 @@ namespace ESFA.DC.ILR1819.ReportService.Service.Service
                 _loadedDataAlready = true;
                 string ukPrnStr = reportServiceContext.Ukprn.ToString();
                 long ukPrn = Convert.ToInt64(ukPrnStr);
-                Organisations organisations = new Organisations(_orgConfiguration.OrgConnectionString);
-                _loadedData = organisations.Org_Details.Where(x => x.UKPRN == ukPrn).Select(x => x.Name).SingleOrDefault();
-                _version = (await organisations.Current_Version.SingleAsync(cancellationToken)).CurrentVersion;
+                DbContextOptions<OrganisationsContext> options = new DbContextOptionsBuilder<OrganisationsContext>().UseSqlServer(_orgConfiguration.OrgConnectionString).Options;
+                using (OrganisationsContext organisations = new OrganisationsContext(options))
+                {
+                    _loadedData = organisations.OrgDetails.Where(x => x.Ukprn == ukPrn).Select(x => x.Name)
+                        .SingleOrDefault();
+                    await GetVersion(organisations, cancellationToken);
+                }
             }
             catch (Exception ex)
             {
@@ -79,8 +83,11 @@ namespace ESFA.DC.ILR1819.ReportService.Service.Service
 
                 if (string.IsNullOrEmpty(_version))
                 {
-                    Organisations organisations = new Organisations(_orgConfiguration.OrgConnectionString);
-                    _version = (await organisations.Current_Version.SingleAsync(cancellationToken)).CurrentVersion;
+                    DbContextOptions<OrganisationsContext> options = new DbContextOptionsBuilder<OrganisationsContext>().UseSqlServer(_orgConfiguration.OrgConnectionString).Options;
+                    using (OrganisationsContext organisations = new OrganisationsContext(options))
+                    {
+                        await GetVersion(organisations, cancellationToken);
+                    }
                 }
             }
             catch (Exception ex)
@@ -93,6 +100,19 @@ namespace ESFA.DC.ILR1819.ReportService.Service.Service
             }
 
             return _version;
+        }
+
+        private async Task GetVersion(OrganisationsContext organisations, CancellationToken cancellationToken)
+        {
+            string version = "Unknown";
+            OrgVersion versionObj = await organisations.OrgVersions.OrderByDescending(x => x.CreatedOn)
+                .FirstOrDefaultAsync(cancellationToken);
+            if (versionObj != null)
+            {
+                version = $"{versionObj.MajorNumber}.{versionObj.MinorNumber}.{versionObj.MaintenanceNumber}";
+            }
+
+            _version = version;
         }
     }
 }
