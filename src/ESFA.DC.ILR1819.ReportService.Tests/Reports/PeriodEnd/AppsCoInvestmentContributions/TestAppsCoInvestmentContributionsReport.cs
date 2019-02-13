be26044c8d719c9df1a43bb7765378bc3444aeb6
Hari Dupati <dupatihari@gmail.com>
2019-02-13 12:16:48 +0000

    69346- Report Model and Tests for AppsCoInvestmentContributionsReport

---
+++ src/ESFA.DC.ILR1819.ReportService.Tests/Reports/PeriodEnd/AppsCoInvestmentContributions/TestAppsCoInvestmentContributionsReport.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.DateTimeProvider.Interface;
+using ESFA.DC.ILR1819.ReportService.Interface.Configuration;
+using ESFA.DC.ILR1819.ReportService.Interface.Context;
+using ESFA.DC.ILR1819.ReportService.Interface.Service;
+using ESFA.DC.ILR1819.ReportService.Service.Mapper.PeriodEnd;
+using ESFA.DC.ILR1819.ReportService.Service.Reports.PeriodEnd;
+using ESFA.DC.ILR1819.ReportService.Service.Service;
+using ESFA.DC.ILR1819.ReportService.Tests.AutoFac;
+using ESFA.DC.ILR1819.ReportService.Tests.Helpers;
+using ESFA.DC.ILR1819.ReportService.Tests.Models;
+using ESFA.DC.IO.Interfaces;
+using ESFA.DC.Logging.Interfaces;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace ESFA.DC.ILR1819.ReportService.Tests.Reports.PeriodEnd.AppsCoInvestmentContributions
+{
+    public sealed class TestAppsCoInvestmentContributionsReport
+    {
+        [Fact]
+        public async Task TestAppsCoInvestmentContributionsReportGeneration()
+        {
+            string csv = string.Empty;
+            DateTime dateTime = DateTime.UtcNow;
+            string filename = $"10033670_1_Apps Co-Investment Contributions Report {dateTime:yyyyMMdd-HHmmss}";
+
+            Mock<IReportServiceContext> reportServiceContextMock = new Mock<IReportServiceContext>();
+            reportServiceContextMock.SetupGet(x => x.JobId).Returns(1);
+            reportServiceContextMock.SetupGet(x => x.SubmissionDateTimeUtc).Returns(DateTime.UtcNow);
+            reportServiceContextMock.SetupGet(x => x.Ukprn).Returns(10033670);
+
+            Mock<ILogger> logger = new Mock<ILogger>();
+            Mock<IDateTimeProvider> dateTimeProviderMock = new Mock<IDateTimeProvider>();
+            Mock<IStreamableKeyValuePersistenceService> storage = new Mock<IStreamableKeyValuePersistenceService>();
+            IValueProvider valueProvider = new ValueProvider();
+            ITopicAndTaskSectionOptions topicsAndTasks = TestConfigurationHelper.GetTopicsAndTasks();
+            storage.Setup(x => x.SaveAsync($"{filename}.csv", It.IsAny<string>(), It.IsAny<CancellationToken>())).Callback<string, string, CancellationToken>((key, value, ct) => csv = value).Returns(Task.CompletedTask);
+
+            dateTimeProviderMock.Setup(x => x.GetNowUtc()).Returns(dateTime);
+            dateTimeProviderMock.Setup(x => x.ConvertUtcToUk(It.IsAny<DateTime>())).Returns(dateTime);
+
+            var report = new AppsCoInvestmentContributionsReport(logger.Object, storage.Object, dateTimeProviderMock.Object, valueProvider, topicsAndTasks);
+
+            await report.GenerateReport(reportServiceContextMock.Object, null, false, CancellationToken.None);
+
+            csv.Should().NotBeNullOrEmpty();
+            File.WriteAllText($"{filename}.csv", csv);
+            TestCsvHelper.CheckCsv(csv, new CsvEntry(new AppsCoInvestmentContributionsMapper(), 1));
+        }
+    }
+}
