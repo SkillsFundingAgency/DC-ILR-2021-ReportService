5d4934e91faa7bd4a58d96239970e0543e04c436
Gayatri <gayatrippatel@hotmail.com>
2019-02-19 11:54:33 +0000

    65413-Trailblazer Apps Occupancy Report

---
+++ src/ESFA.DC.ILR1819.ReportService.Service/Comparer/TrailblazerAppsOccupancyModelComparer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR1819.ReportService.Model.ReportModels;
+
+namespace ESFA.DC.ILR1819.ReportService.Service.Comparer
+{
+    public class TrailblazerAppsOccupancyModelComparer : IComparer<TrailblazerAppsOccupancyModel>
+    {
+        public int Compare(TrailblazerAppsOccupancyModel x, TrailblazerAppsOccupancyModel y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+
+            if (x == null)
+            {
+                return -1;
+            }
+
+            if (y == null)
+            {
+                return 1;
+            }
+
+            if (x == y)
+            {
+                return 0;
+            }
+
+            int cmp = string.CompareOrdinal(x.LearnRefNumber, y.LearnRefNumber);
+            if (cmp != 0)
+            {
+                return cmp;
+            }
+
+            if (x.AimSeqNumber > y.AimSeqNumber)
+            {
+                return 1;
+            }
+
+            if (x.AimSeqNumber < y.AimSeqNumber)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
