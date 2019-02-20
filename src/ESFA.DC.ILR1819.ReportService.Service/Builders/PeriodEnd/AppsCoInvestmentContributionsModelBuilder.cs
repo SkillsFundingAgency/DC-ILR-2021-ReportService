7c0197bc3e448bebcc7e3509abafb83d6bb8acf2
Hari Dupati <dupatihari@gmail.com>
2019-02-20 14:14:50 +0000

    69907-ILR Provider service ILRInfoForAppsCoInvestmentReport

---
+++ src/ESFA.DC.ILR1819.ReportService.Service/Builders/PeriodEnd/AppsCoInvestmentContributionsModelBuilder.cs
@@ -0,0 +1,8 @@
+﻿using ESFA.DC.ILR1819.ReportService.Interface.Builders.PeriodEnd;
+
+namespace ESFA.DC.ILR1819.ReportService.Service.Builders.PeriodEnd
+{
+    public class AppsCoInvestmentContributionsModelBuilder : IAppsCoInvestmentContributionsModelBuilder
+    {
+    }
+}
