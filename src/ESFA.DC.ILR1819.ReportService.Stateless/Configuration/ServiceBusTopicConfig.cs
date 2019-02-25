61d64f925ef1e7bdd87aaf9562690a7de84ad534
Gayatri <gayatrippatel@hotmail.com>
2019-02-25 14:23:55 +0000

    69935-Increasing service callback timespan for to cop with reports

--- src/ESFA.DC.ILR1819.ReportService.Stateless/Configuration/ServiceBusTopicConfig.cs
+++ src/ESFA.DC.ILR1819.ReportService.Stateless/Configuration/ServiceBusTopicConfig.cs
@@ -1,11 +1,13 @@
-﻿using ESFA.DC.Queueing;
+﻿using System;
+using System.Fabric.Query;
+using ESFA.DC.Queueing;
 
 namespace ESFA.DC.ILR1819.ReportService.Stateless.Configuration
 {
     public class ServiceBusTopicConfig : TopicConfiguration
     {
-        public ServiceBusTopicConfig(string connectionString, string topicName, string subscriptionName, int maxConcurrentCalls, int minimumBackoffSeconds = 5, int maximumBackoffSeconds = 50, int maximumRetryCount = 10)
-            : base(connectionString, topicName, subscriptionName, maxConcurrentCalls, minimumBackoffSeconds, maximumBackoffSeconds, maximumRetryCount)
+        public ServiceBusTopicConfig(string connectionString, string topicName, string subscriptionName, int maxConcurrentCalls, int minimumBackoffSeconds = 5, int maximumBackoffSeconds = 50, int maximumRetryCount = 10, TimeSpan? maximumCallbackTimeSpan = null)
+            : base(connectionString, topicName, subscriptionName, maxConcurrentCalls, minimumBackoffSeconds, maximumBackoffSeconds, maximumRetryCount, maximumCallbackTimeSpan = maximumCallbackTimeSpan ?? new TimeSpan(0, 30, 0))
         {
         }
     }
