5263074a56ccdb230687f1da6354578b8023908e
Gayatri <gayatrippatel@hotmail.com>
2019-02-28 14:21:56 +0000

    66229-adding extra sorting option for PriceEpisodeStartDate

--- src/ESFA.DC.ILR1819.ReportService.Service/Comparer/AppsIndicativeEarningsModelComparer.cs
+++ src/ESFA.DC.ILR1819.ReportService.Service/Comparer/AppsIndicativeEarningsModelComparer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ESFA.DC.ILR1819.ReportService.Model.ReportModels;
 
 namespace ESFA.DC.ILR1819.ReportService.Service.Comparer
@@ -41,6 +42,26 @@ namespace ESFA.DC.ILR1819.ReportService.Service.Comparer
             if (x.AimSeqNumber < y.AimSeqNumber)
             {
                 return -1;
+            }
+
+            if (DateTime.TryParse(x.PriceEpisodeStartDate, out DateTime xPriceEpisodeStartDate) &&
+                DateTime.TryParse(y.PriceEpisodeStartDate, out DateTime yPriceEpisodeStartDate))
+            {
+                int cmpPriceEpisodeStartDates = DateTime.Compare(xPriceEpisodeStartDate, yPriceEpisodeStartDate);
+                if (cmpPriceEpisodeStartDates != 0)
+                {
+                    return cmpPriceEpisodeStartDates;
+                }
+
+                if (cmpPriceEpisodeStartDates > 0)
+                {
+                    return 1;
+                }
+
+                if (cmpPriceEpisodeStartDates < 0)
+                {
+                    return -1;
+                }
             }
 
             return 0;
