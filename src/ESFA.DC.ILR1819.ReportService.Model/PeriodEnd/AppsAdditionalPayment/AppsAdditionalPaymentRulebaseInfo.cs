1385dca59091865d4298cc2f875b24c559fccf85
Hari Dupati <dupatihari@gmail.com>
2019-03-01 11:27:08 +0000

    70532-FM36 Provider for Apps Additional Payments Report

---
+++ src/ESFA.DC.ILR1819.ReportService.Model/PeriodEnd/AppsAdditionalPayment/AppsAdditionalPaymentRulebaseInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR1819.ReportService.Model.PeriodEnd.AppsAdditionalPayment
+{
+    public class AppsAdditionalPaymentRulebaseInfo
+    {
+        public int UkPrn { get; set; }
+
+        public List<AECApprenticeshipPriceEpisodePeriodisedValuesInfo> AECApprenticeshipPriceEpisodePeriodisedValues { get; set; }
+
+        public List<AECLearningDeliveryInfo> AECLearningDeliveries { get; set; }
+    }
+}
