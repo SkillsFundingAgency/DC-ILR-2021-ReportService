60a12f9b0b1ffecc967e4126328ea5085597d27f
Hari Dupati <dupatihari@gmail.com>
2019-03-15 13:54:31 +0000

    71225-AppsMonthlyPayment FM36 Provider

--- src/ESFA.DC.ILR1819.ReportService.Interface/Service/IFM36ProviderService.cs
+++ src/ESFA.DC.ILR1819.ReportService.Interface/Service/IFM36ProviderService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using ESFA.DC.ILR1819.ReportService.Interface.Context;
 using ESFA.DC.ILR1819.ReportService.Model.PeriodEnd.AppsAdditionalPayment;
+using ESFA.DC.ILR1819.ReportService.Model.PeriodEnd.AppsMonthlyPayment;
 
 namespace ESFA.DC.ILR1819.ReportService.Interface.Service
 {
@@ -11,5 +12,7 @@ namespace ESFA.DC.ILR1819.ReportService.Interface.Service
         Task<FM36Global> GetFM36Data(IReportServiceContext reportServiceContext, CancellationToken cancellationToken);
 
         Task<AppsAdditionalPaymentRulebaseInfo> GetFM36DataForAppsAdditionalPaymentReportAsync(int ukPrn, CancellationToken cancellationToken);
+
+        Task<AppsMonthlyPaymentRulebaseInfo> GetFM36DataForAppsMonthlyPaymentReportAsync(int ukPrn, CancellationToken cancellationToken);
     }
 }
---
+++ src/ESFA.DC.ILR1819.ReportService.Model/PeriodEnd/AppsMonthlyPayment/AppsMonthlyPaymentRulebaseInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR1819.ReportService.Model.PeriodEnd.AppsMonthlyPayment
+{
+    public class AppsMonthlyPaymentRulebaseInfo
+    {
+        public int UkPrn { get; set; }
+
+        public string LearnRefNumber { get; set; }
+
+        public ICollection<AECApprenticeshipPriceEpisodeInfo> AECApprenticeshipPriceEpisodes { get; set; }
+    }
+}
