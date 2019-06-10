77cf1a38e1408a4e6abffb16707e57ee0697c8b3
Hari Dupati <dupatihari@gmail.com>
2019-06-10 13:52:14 +0100

    76048-unit tests fix

--- src/ESFA.DC.ILR.ReportService.Service.Tests/ReportsProviderTests.cs
+++ src/ESFA.DC.ILR.ReportService.Service.Tests/ReportsProviderTests.cs
@@ -1,196 +1,196 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using ESFA.DC.ILR.ReportService.Interface.Context;
-//using ESFA.DC.ILR.ReportService.Interface.Reports;
-//using ESFA.DC.ILR.ReportService.Service.Service;
-//using ESFA.DC.Logging.Interfaces;
-//using FluentAssertions;
-//using Moq;
-//using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.ReportService.Interface.Context;
+using ESFA.DC.ILR.ReportService.Interface.Reports;
+using ESFA.DC.ILR.ReportService.Service.Service;
+using ESFA.DC.Logging.Interfaces;
+using FluentAssertions;
+using Moq;
+using Xunit;
 
-//namespace ESFA.DC.ILR.ReportService.Service.Tests
-//{
-//    public class ReportsProviderTests
-//    {
-//        [Fact]
-//        public void ProvideReportsForContext_NoneRegistered()
-//        {
-//            var reportOne = "ReportOne";
-//            var reportTwo = "ReportTwo";
-//            var reportThree = "ReportThree";
+namespace ESFA.DC.ILR.ReportService.Service.Tests
+{
+    public class ReportsProviderTests
+    {
+        [Fact]
+        public void ProvideReportsForContext_NoneRegistered()
+        {
+            var reportOne = "ReportOne";
+            var reportTwo = "ReportTwo";
+            var reportThree = "ReportThree";
 
-//            var reports = new List<IReport>();
-//            var loggerMock = new Mock<ILogger>();
+            var reports = new List<IReport>();
+            var loggerMock = new Mock<ILogger>();
 
-//            var reportServiceContext = new Mock<IReportServiceContext>();
+            var reportServiceContext = new Mock<IReportServiceContext>();
 
-//            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportOne, reportTwo, reportThree });
+            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportOne, reportTwo, reportThree });
 
-//            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
+            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
 
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportOne}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportTwo}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportThree}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//        }
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportOne}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportTwo}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportThree}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+        }
 
-//        [Fact]
-//        public void ProvideReportsForContext_NoTasks()
-//        {
-//            var reportOneTaskName = "ReportOne";
-//            var reportTwoTaskName = "ReportTwo";
-//            var reportThreeTaskName = "ReportThree";
+        [Fact]
+        public void ProvideReportsForContext_NoTasks()
+        {
+            var reportOneTaskName = "ReportOne";
+            var reportTwoTaskName = "ReportTwo";
+            var reportThreeTaskName = "ReportThree";
 
-//            var reportOneMock = new Mock<IReport>();
-//            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
-//            var reportTwoMock = new Mock<IReport>();
-//            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
-//            var reportThreeMock = new Mock<IReport>();
-//            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
+            var reportOneMock = new Mock<IReport>();
+            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
+            var reportTwoMock = new Mock<IReport>();
+            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
+            var reportThreeMock = new Mock<IReport>();
+            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
 
-//            var reports = new List<IReport>()
-//            {
-//                reportOneMock.Object,
-//                reportTwoMock.Object,
-//                reportThreeMock.Object,
-//            };
+            var reports = new List<IReport>()
+            {
+                reportOneMock.Object,
+                reportTwoMock.Object,
+                reportThreeMock.Object,
+            };
 
-//            var loggerMock = new Mock<ILogger>();
+            var loggerMock = new Mock<ILogger>();
 
-//            var reportServiceContext = new Mock<IReportServiceContext>();
+            var reportServiceContext = new Mock<IReportServiceContext>();
 
-//            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>());
+            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>());
 
-//            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
-//        }
+            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
+        }
 
-//        [Fact]
-//        public void ProvideReportsForContext_Matching()
-//        {
-//            var reportOneTaskName = "ReportOne";
-//            var reportTwoTaskName = "ReportTwo";
-//            var reportThreeTaskName = "ReportThree";
+        [Fact]
+        public void ProvideReportsForContext_Matching()
+        {
+            var reportOneTaskName = "ReportOne";
+            var reportTwoTaskName = "ReportTwo";
+            var reportThreeTaskName = "ReportThree";
 
-//            var reportOneMock = new Mock<IReport>();
-//            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
-//            var reportTwoMock = new Mock<IReport>();
-//            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
-//            var reportThreeMock = new Mock<IReport>();
-//            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
+            var reportOneMock = new Mock<IReport>();
+            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
+            var reportTwoMock = new Mock<IReport>();
+            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
+            var reportThreeMock = new Mock<IReport>();
+            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
 
-//            var reports = new List<IReport>()
-//            {
-//                reportOneMock.Object,
-//                reportTwoMock.Object,
-//                reportThreeMock.Object,
-//            };
+            var reports = new List<IReport>()
+            {
+                reportOneMock.Object,
+                reportTwoMock.Object,
+                reportThreeMock.Object,
+            };
 
-//            var loggerMock = new Mock<ILogger>();
+            var loggerMock = new Mock<ILogger>();
 
-//            var reportServiceContext = new Mock<IReportServiceContext>();
+            var reportServiceContext = new Mock<IReportServiceContext>();
 
-//            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportOneTaskName, reportTwoTaskName, reportThreeTaskName });
+            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportOneTaskName, reportTwoTaskName, reportThreeTaskName });
 
-//            var matchingReports = NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object);
+            var matchingReports = NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object);
 
-//            matchingReports.Should().Contain(reportOneMock.Object);
-//            matchingReports.Should().Contain(reportTwoMock.Object);
-//            matchingReports.Should().Contain(reportThreeMock.Object);
-//        }
+            matchingReports.Should().Contain(reportOneMock.Object);
+            matchingReports.Should().Contain(reportTwoMock.Object);
+            matchingReports.Should().Contain(reportThreeMock.Object);
+        }
 
-//        [Fact]
-//        public void ProvideReportsForContext_NoneMatching()
-//        {
-//            var reportOneTaskName = "ReportOne";
-//            var reportTwoTaskName = "ReportTwo";
-//            var reportThreeTaskName = "ReportThree";
-//            var reportFourTaskName = "ReportFour";
-//            var reportFiveTaskName = "ReportFive";
-//            var reportSixTaskName = "ReportSix";
+        [Fact]
+        public void ProvideReportsForContext_NoneMatching()
+        {
+            var reportOneTaskName = "ReportOne";
+            var reportTwoTaskName = "ReportTwo";
+            var reportThreeTaskName = "ReportThree";
+            var reportFourTaskName = "ReportFour";
+            var reportFiveTaskName = "ReportFive";
+            var reportSixTaskName = "ReportSix";
 
-//            var reportOneMock = new Mock<IReport>();
-//            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
-//            var reportTwoMock = new Mock<IReport>();
-//            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
-//            var reportThreeMock = new Mock<IReport>();
-//            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
+            var reportOneMock = new Mock<IReport>();
+            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
+            var reportTwoMock = new Mock<IReport>();
+            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
+            var reportThreeMock = new Mock<IReport>();
+            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
 
-//            var reports = new List<IReport>()
-//            {
-//                reportOneMock.Object,
-//                reportTwoMock.Object,
-//                reportThreeMock.Object,
-//            };
+            var reports = new List<IReport>()
+            {
+                reportOneMock.Object,
+                reportTwoMock.Object,
+                reportThreeMock.Object,
+            };
 
-//            var loggerMock = new Mock<ILogger>();
+            var loggerMock = new Mock<ILogger>();
 
-//            var reportServiceContext = new Mock<IReportServiceContext>();
+            var reportServiceContext = new Mock<IReportServiceContext>();
 
-//            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportFourTaskName, reportFiveTaskName, reportSixTaskName });
+            reportServiceContext.SetupGet(c => c.Tasks).Returns(new List<string>() { reportFourTaskName, reportFiveTaskName, reportSixTaskName });
 
-//            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
+            NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object).Should().BeEmpty();
 
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFourTaskName}", null, 0, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFiveTaskName}", null, 0, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportSixTaskName}", null, 0, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//        }
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFourTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFiveTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportSixTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+        }
 
-//        [Fact]
-//        public void ProvideReportsForContext_SomeMatching()
-//        {
-//            var reportOneTaskName = "ReportOne";
-//            var reportTwoTaskName = "ReportTwo";
-//            var reportThreeTaskName = "ReportThree";
-//            var reportFourTaskName = "ReportFour";
-//            var reportFiveTaskName = "ReportFive";
-//            var reportSixTaskName = "ReportSix";
+        [Fact]
+        public void ProvideReportsForContext_SomeMatching()
+        {
+            var reportOneTaskName = "ReportOne";
+            var reportTwoTaskName = "ReportTwo";
+            var reportThreeTaskName = "ReportThree";
+            var reportFourTaskName = "ReportFour";
+            var reportFiveTaskName = "ReportFive";
+            var reportSixTaskName = "ReportSix";
 
-//            var reportOneMock = new Mock<IReport>();
-//            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
-//            var reportTwoMock = new Mock<IReport>();
-//            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
-//            var reportThreeMock = new Mock<IReport>();
-//            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
+            var reportOneMock = new Mock<IReport>();
+            reportOneMock.SetupGet(r => r.ReportTaskName).Returns(reportOneTaskName);
+            var reportTwoMock = new Mock<IReport>();
+            reportTwoMock.SetupGet(r => r.ReportTaskName).Returns(reportTwoTaskName);
+            var reportThreeMock = new Mock<IReport>();
+            reportThreeMock.SetupGet(r => r.ReportTaskName).Returns(reportThreeTaskName);
 
-//            var reports = new List<IReport>()
-//            {
-//                reportOneMock.Object,
-//                reportTwoMock.Object,
-//                reportThreeMock.Object,
-//            };
+            var reports = new List<IReport>()
+            {
+                reportOneMock.Object,
+                reportTwoMock.Object,
+                reportThreeMock.Object,
+            };
 
-//            var loggerMock = new Mock<ILogger>();
+            var loggerMock = new Mock<ILogger>();
 
-//            var reportServiceContext = new Mock<IReportServiceContext>();
+            var reportServiceContext = new Mock<IReportServiceContext>();
 
-//            reportServiceContext
-//                .SetupGet(c => c.Tasks)
-//                .Returns(new List<string>()
-//                { 
-//                    reportOneTaskName, 
-//                    reportTwoTaskName,
-//                    reportThreeTaskName,
-//                    reportFourTaskName,
-//                    reportFiveTaskName,
-//                    reportSixTaskName
-//                });
+            reportServiceContext
+                .SetupGet(c => c.Tasks)
+                .Returns(new List<string>()
+                {
+                    reportOneTaskName,
+                    reportTwoTaskName,
+                    reportThreeTaskName,
+                    reportFourTaskName,
+                    reportFiveTaskName,
+                    reportSixTaskName
+                });
 
-//            var matchingReports = NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object);
-            
-//            matchingReports.Should().Contain(reportOneMock.Object);
-//            matchingReports.Should().Contain(reportTwoMock.Object);
-//            matchingReports.Should().Contain(reportThreeMock.Object);
+            var matchingReports = NewProvider(reports, loggerMock.Object).ProvideReportsForContext(reportServiceContext.Object);
 
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFourTaskName}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFiveTaskName}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportSixTaskName}", null, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
-//        }
+            matchingReports.Should().Contain(reportOneMock.Object);
+            matchingReports.Should().Contain(reportTwoMock.Object);
+            matchingReports.Should().Contain(reportThreeMock.Object);
 
-//        private ReportsProvider NewProvider(IList<IReport> reports = null, ILogger logger = null)
-//        {
-//            return new ReportsProvider(reports, logger);
-//        }
-//    }
-//}
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFourTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportFiveTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+            loggerMock.Verify(l => l.LogWarning($"Missing Report Task - {reportSixTaskName}", null, -1, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+        }
+
+        private ReportsProvider NewProvider(IList<IReport> reports = null, ILogger logger = null)
+        {
+            return new ReportsProvider(reports, logger);
+        }
+    }
+}
