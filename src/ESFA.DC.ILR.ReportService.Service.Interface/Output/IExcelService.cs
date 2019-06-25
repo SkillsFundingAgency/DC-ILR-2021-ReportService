899fe6cf9b31fa20838ef9878fcf85db4c0fa350
Mike Childs <mike@mjcdev.co.uk>
2019-06-25 15:25:41 +0100

    Csv Service Initial Implementation, Excel Interface Created

---
+++ src/ESFA.DC.ILR.ReportService.Service.Interface/Output/IExcelService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Aspose.Cells;
+
+namespace ESFA.DC.ILR.ReportService.Service.Interface.Output
+{
+    public interface IExcelService
+    {
+        Task WriteWorkbookAsync(Workbook workbook, string fileName, string container, CancellationToken cancellationToken);
+    }
+}
