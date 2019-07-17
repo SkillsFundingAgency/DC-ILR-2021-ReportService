c8fb723ed27dee71febf10b8aa580d59b14aefe8
Mike Childs <mike@mjcdev.co.uk>
2019-07-17 11:41:37 +0100

    Abstract File Service Provider

--- src/ESFA.DC.ILR.ReportService.Data/Providers/IlrFileServiceProvider.cs
+++ src/ESFA.DC.ILR.ReportService.Data/Providers/IlrFileServiceProvider.cs
@@ -2,32 +2,20 @@
 using System.Threading.Tasks;
 using ESFA.DC.FileService.Interface;
 using ESFA.DC.ILR.Model;
+using ESFA.DC.ILR.ReportService.Data.Providers.Abstract;
 using ESFA.DC.ILR.ReportService.Service.Interface;
 using ESFA.DC.Serialization.Interfaces;
 
 namespace ESFA.DC.ILR.ReportService.Data.Providers
 {
-    public class IlrFileServiceProvider : IExternalDataProvider
+    public class IlrFileServiceProvider : AbstractFileServiceProvider, IExternalDataProvider
     {
-        private readonly IFileService _fileService;
-        private readonly IXmlSerializationService _xmlSerializationService;
-
         public IlrFileServiceProvider(IFileService fileService, IXmlSerializationService xmlSerializationService)
+        : base(fileService, xmlSerializationService)
         {
-            _fileService = fileService;
-            _xmlSerializationService = xmlSerializationService;
         }
 
         public async Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
-        {
-            Message message;
-
-            using (var fileStream = await _fileService.OpenReadStreamAsync(reportServiceContext.Filename, reportServiceContext.Container, cancellationToken))
-            {
-                message = _xmlSerializationService.Deserialize<Message>(fileStream);
-            }
-
-            return message;
-        }
+            => await ProvideAsync<Message>(reportServiceContext.Filename, reportServiceContext.Container, cancellationToken);
     }
 }
--- src/ESFA.DC.ILR.ReportService.Data/Providers/IlrValidationErrorsProvider.cs
+++ src/ESFA.DC.ILR.ReportService.Data/Providers/IlrValidationErrorsProvider.cs
@@ -2,33 +2,21 @@
 using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.FileService.Interface;
+using ESFA.DC.ILR.ReportService.Data.Providers.Abstract;
 using ESFA.DC.ILR.ReportService.Service.Interface;
 using ESFA.DC.ILR.ValidationErrors.Interface.Models;
 using ESFA.DC.Serialization.Interfaces;
 
 namespace ESFA.DC.ILR.ReportService.Data.Providers
 {
-    public class IlrValidationErrorsProvider : IExternalDataProvider
+    public class IlrValidationErrorsProvider : AbstractFileServiceProvider, IExternalDataProvider
     {
-        private readonly IFileService _fileService;
-        private readonly IJsonSerializationService _jsonSerializationService;
-
         public IlrValidationErrorsProvider(IFileService fileService, IJsonSerializationService jsonSerializationService)
+            : base(fileService, jsonSerializationService)
         {
-            _fileService = fileService;
-            _jsonSerializationService = jsonSerializationService;
         }
 
-       public async Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
-        {
-            List<ValidationError> message;
-
-            using (var fileStream = await _fileService.OpenReadStreamAsync(reportServiceContext.ValidationErrorsKey, reportServiceContext.Container, cancellationToken))
-            {
-                message = _jsonSerializationService.Deserialize<List<ValidationError>>(fileStream);
-            }
-
-            return message;
-        }
+        public async Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
+            => ProvideAsync<List<ValidationError>>(reportServiceContext.ValidationErrorsKey, reportServiceContext.Container, cancellationToken);
     }
 }
