15fa3107423e0a535ead45471d0eef7e995c45ae
Mike Childs <mike@mjcdev.co.uk>
2019-07-24 10:00:48 +0100

    Funding Summary Report Depends On

--- src/ESFA.DC.ILR.ReportService.Reports/Funding/FundingSummary/FundingSummaryReport.cs
+++ src/ESFA.DC.ILR.ReportService.Reports/Funding/FundingSummary/FundingSummaryReport.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.ILR.ReportService.Reports.Abstract;
+using ESFA.DC.ILR.ReportService.Reports.Funding.Interface;
 using ESFA.DC.ILR.ReportService.Service.Interface;
 
 namespace ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary
@@ -18,7 +19,11 @@ namespace ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary
         public IEnumerable<Type> DependsOn
             => new[] 
             {
+                DependentDataCatalog.Fm25,
                 DependentDataCatalog.Fm35,
+                DependentDataCatalog.Fm36,
+                DependentDataCatalog.Fm81,
+                DependentDataCatalog.Fm99,
             };
 
         public Task<IEnumerable<string>> GenerateAsync(
