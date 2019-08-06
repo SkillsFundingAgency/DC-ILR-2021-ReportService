284fff8f235c793c8573a3c2933cdba649a98369
Cory-Wynn <CoryWynn1993@gmail.com>
2019-08-06 14:37:56 +0000

    85019 - Invalid serilization fix

--- src/ESFA.DC.ILR.ReportService.Data/Providers/InvalidIlrFileServiceProvider.cs
+++ src/ESFA.DC.ILR.ReportService.Data/Providers/InvalidIlrFileServiceProvider.cs
@@ -4,28 +4,49 @@ using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.FileService.Interface;
 using ESFA.DC.ILR.Model.Loose;
-using ESFA.DC.ILR.ReportService.Data.Providers.Abstract;
 using ESFA.DC.ILR.ReportService.Service.Interface;
 using ESFA.DC.Serialization.Interfaces;
 
 namespace ESFA.DC.ILR.ReportService.Data.Providers
 {
-    public class InvalidIlrFileServiceProvider : AbstractFileServiceProvider, IExternalDataProvider
+    public class InvalidIlrFileServiceProvider : IExternalDataProvider
     {
-        public InvalidIlrFileServiceProvider(IFileService fileService, IXmlSerializationService xmlSerializationService)
-        : base(fileService, xmlSerializationService)
+        private readonly IFileService _fileService;
+        private readonly IXmlSerializationService _xmlSerializationService;
+        private readonly IJsonSerializationService _jsonSerializationService;
+
+        public InvalidIlrFileServiceProvider(IFileService fileService, IXmlSerializationService xmlSerializationService, IJsonSerializationService jsonSerializationService)
         {
+            _fileService = fileService;
+            _xmlSerializationService = xmlSerializationService;
+            _jsonSerializationService = jsonSerializationService;
         }
 
         public async Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
         {
-            var message = await ProvideAsync<Message>(reportServiceContext.OriginalFilename, reportServiceContext.Container, cancellationToken) as Message;
-            var invalidLearnRefNumbers = await ProvideAsync<List<string>>(reportServiceContext.InvalidLearnRefNumbersKey, reportServiceContext.Container, cancellationToken) as List<string>;
+            var message = await ProvideXmlAsync<Message>(reportServiceContext.OriginalFilename, reportServiceContext.Container, cancellationToken) as Message;
+            var invalidLearnRefNumbers = await ProvideJsonAsync<List<string>>(reportServiceContext.InvalidLearnRefNumbersKey, reportServiceContext.Container, cancellationToken) as List<string>;
 
             message.Learner = message.Learner.Where(l => invalidLearnRefNumbers.Contains(l.LearnRefNumber)).ToArray();
             message.LearnerDestinationandProgression = message.LearnerDestinationandProgression.Where(l => invalidLearnRefNumbers.Contains(l.LearnRefNumber)).ToArray();
 
             return message;
+        }
+
+        private async Task<object> ProvideXmlAsync<T>(string fileName, string container, CancellationToken cancellationToken)
+        {
+            using (var fileStream = await _fileService.OpenReadStreamAsync(fileName, container, cancellationToken))
+            {
+                return _xmlSerializationService.Deserialize<T>(fileStream);
+            }
+        }
+
+        private async Task<object> ProvideJsonAsync<T>(string fileName, string container, CancellationToken cancellationToken)
+        {
+            using (var fileStream = await _fileService.OpenReadStreamAsync(fileName, container, cancellationToken))
+            {
+                return _jsonSerializationService.Deserialize<T>(fileStream);
+            }
         }
     }
 }
