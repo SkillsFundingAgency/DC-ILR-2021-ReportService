fd0ef710b5be0ec789a213936ddfe2e67ce75642
Mike Childs <mike@mjcdev.co.uk>
2019-08-13 11:00:28 +0100

    Render Service no longer renders a fund line group total where there is only one fund line group in the funding sub categories fundline group collection

--- src/ESFA.DC.ILR.ReportService.Reports.Tests/Funding/FundingSummary/FundingSummaryReportRenderServiceTests.cs
+++ src/ESFA.DC.ILR.ReportService.Reports.Tests/Funding/FundingSummary/FundingSummaryReportRenderServiceTests.cs
@@ -23,16 +23,13 @@ namespace ESFA.DC.ILR.ReportService.Reports.Tests.Funding.FundingSummary
                     .Select(l => (IFundingCategory)new FundingCategory("Funding Category Title", currentPeriod, Enumerable.Range(1, 10)
                         .Select(k => (IFundingSubCategory)new FundingSubCategory("Funding Sub Category Title", currentPeriod)
                         {
-                            FundLineGroups = Enumerable.Range(1, 3)
+                            FundLineGroups = Enumerable.Range(1, 2)
                                 .Select(i =>
                                 {
-                                    return (IFundLineGroup) new FundLineGroup("FundLineGroup", currentPeriod, FundingDataSources.FM35,
-                                        new string[] { }, null)
+                                    return (IFundLineGroup) new FundLineGroup("FundLineGroup", currentPeriod, FundingDataSources.FM35, new string[] { }, null)
                                     {
                                         FundLines = Enumerable.Range(0, 5)
-                                            .Select(j => (IFundLine) new FundLine(currentPeriod, "Title", 1.1111m, 2.2222m, 3.3333m,
-                                                4.4444m,
-                                                5.5555m, 6.6666m, 7.7777m, 8.8888m, 9.9999m, 10.1010m, 11.1111m, 12.1212m))
+                                            .Select(j => (IFundLine) new FundLine(currentPeriod, "Title", 1.1111m, 2.2222m, 3.3333m, 4.4444m, 5.5555m, 6.6666m, 7.7777m, 8.8888m, 9.9999m, 10.1010m, 11.1111m, 12.1212m))
                                             .ToList()
                                     };
                                 }).ToList()
