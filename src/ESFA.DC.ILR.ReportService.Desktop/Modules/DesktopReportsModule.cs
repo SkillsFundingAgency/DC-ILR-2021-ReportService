2750d0f3ae0a67fbfcf4198607066d55273ed15b
Ashley Beecham <ash@momentoussolutions.co.uk>
2019-08-21 16:14:19 +0100

    85525 - Adding override registartion for desktop

--- src/ESFA.DC.ILR.ReportService.Desktop/Modules/DesktopReportsModule.cs
+++ src/ESFA.DC.ILR.ReportService.Desktop/Modules/DesktopReportsModule.cs
@@ -3,9 +3,14 @@ using ESFA.DC.ILR.ReportService.Modules;
 using ESFA.DC.ILR.ReportService.Reports.Funding;
 using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary;
 using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Desktop;
+using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Devolved;
+using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Devolved.Desktop;
+using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Devolved.Model;
+using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Devolved.Model.Interface;
 using ESFA.DC.ILR.ReportService.Reports.Funding.FundingSummary.Model.Interface;
 using ESFA.DC.ILR.ReportService.Reports.Funding.Interface;
 using ESFA.DC.ILR.ReportService.Service.Interface;
+using System.Collections.Generic;
 
 namespace ESFA.DC.ILR.ReportService.Desktop.Modules
 {
@@ -17,6 +22,13 @@ namespace ESFA.DC.ILR.ReportService.Desktop.Modules
             containerBuilder.RegisterType<DesktopFundingSummaryReportModelBuilder>().As<IModelBuilder<IFundingSummaryReport>>();
             containerBuilder.RegisterType<FundingSummaryReportRenderService>().As<IRenderService<IFundingSummaryReport>>();
             containerBuilder.RegisterType<PeriodisedValuesLookupProvider>().As<IPeriodisedValuesLookupProvider>();
+        }
+
+        protected override void RegisterDevolvedAdultEducationFundingSummaryReport(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<DevolvedAdultEducationFundingSummaryReport>().As<IReport>();
+            containerBuilder.RegisterType<DesktopDevolvedAdultEducationFundingSummaryModelBuilder>().As<IModelBuilder<IEnumerable<DevolvedAdultEducationFundingSummaryReportModel>>>();
+            containerBuilder.RegisterType<DevolvedAdultEducationFundingSummaryReportRenderService>().As<IRenderService<IDevolvedAdultEducationFundingSummaryReport>>();
         }
     }
 }
