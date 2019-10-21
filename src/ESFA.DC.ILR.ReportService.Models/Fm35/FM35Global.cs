eb1d556199cb0e4d37ba3f718be37cf8bdeac22a
Cory-Wynn <CoryWynn1993@gmail.com>
2019-10-21 13:37:08 +0000

    84325 Cleaned up Fm35 models

--- src/ESFA.DC.ILR.ReportService.Models/Fm35/FM35Global.cs
+++ src/ESFA.DC.ILR.ReportService.Models/Fm35/FM35Global.cs
@@ -4,18 +4,6 @@ namespace ESFA.DC.ILR.ReportService.Models.Fm35
 {
     public class FM35Global
     {
-        public int UKPRN { get; set; }
-
-        public string CurFundYr { get; set; }
-
-        public string LARSVersion { get; set; }
-
-        public string OrgVersion { get; set; }
-
-        public string PostcodeDisadvantageVersion { get; set; }
-
-        public string RulebaseVersion { get; set; }
-
         public List<FM35Learner> Learners { get; set; }
     }
 }
