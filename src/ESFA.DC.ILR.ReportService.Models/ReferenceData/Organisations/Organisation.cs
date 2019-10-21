d450666bd2eb75e3925347f791c7bff20c4f6e91
Cory-Wynn <CoryWynn1993@gmail.com>
2019-10-21 14:15:47 +0000

    84325 cleaned up org models

--- src/ESFA.DC.ILR.ReportService.Models/ReferenceData/Organisations/Organisation.cs
+++ src/ESFA.DC.ILR.ReportService.Models/ReferenceData/Organisations/Organisation.cs
@@ -8,14 +8,6 @@ namespace ESFA.DC.ILR.ReportService.Models.ReferenceData.Organisations
 
         public string Name { get; set; }
 
-        public bool? PartnerUKPRN { get; set; }
-
-        public string LegalOrgType { get; set; }
-
-        public List<OrganisationCampusIdentifier> CampusIdentifers { get; set; }
-
-        public List<OrganisationFunding> OrganisationFundings { get; set; }
-
         public List<OrganisationCoFRemoval> OrganisationCoFRemovals { get; set; }
     }
 }
--- src/ESFA.DC.ILR.ReportService.Models/ReferenceData/Organisations/SpecialistResource.cs
+++
@@ -1,9 +0,0 @@
-﻿using ESFA.DC.ILR.ReportService.Models.ReferenceData.Abstract;
-
-namespace ESFA.DC.ILR.ReportService.Models.ReferenceData.Organisations
-{
-    public class SpecialistResource : AbstractTimeBoundedEntity
-    {
-        public bool IsSpecialistResource { get; set; }
-    }
-}
