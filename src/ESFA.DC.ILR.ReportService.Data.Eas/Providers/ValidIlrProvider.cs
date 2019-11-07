7efca384271a75911b369b4182a7f8b60da79c86
Cory-Wynn <CoryWynn1993@gmail.com>
2019-11-07 12:05:55 +0000

    84325 Eas reference data provider

---
+++ src/ESFA.DC.ILR.ReportService.Data.Eas/Providers/ValidIlrProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model;
+using ESFA.DC.ILR.ReportService.Service.Interface;
+
+namespace ESFA.DC.ILR.ReportService.Data.Eas.Providers
+{
+    public class ValidIlrProvider : IExternalDataProvider
+    {
+        public async Task<object> ProvideAsync(IReportServiceContext reportServiceContext, CancellationToken cancellationToken)
+        {
+            return new Message();
+        }
+    }
+}
