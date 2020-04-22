3f5fe9df93ac8936dea349b6e5b572f8642529fe
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-04-22 14:11:30 +0100

    1619 Summary of Funding by Student 2021 uplift

--- src/ESFA.DC.ILR.ReportService.Reports/Funding/SixteenToNineteen/SummaryOfFundingByStudent/SummaryOfFundingByStudentReportClassMap.cs
+++ src/ESFA.DC.ILR.ReportService.Reports/Funding/SixteenToNineteen/SummaryOfFundingByStudent/SummaryOfFundingByStudentReportClassMap.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper.Configuration;
+using ESFA.DC.ILR.ReportService.Reports.Service.Converters;
 
 namespace ESFA.DC.ILR.ReportService.Reports.Funding.SixteenToNineteen.SummaryOfFundingByStudent
 {
@@ -12,13 +13,14 @@ namespace ESFA.DC.ILR.ReportService.Reports.Funding.SixteenToNineteen.SummaryOfF
             Map(m => m.Learner.LearnRefNumber).Name(@"Learner reference number").Index(++index);
             Map(m => m.Learner.FamilyName).Name(@"Family name").Index(++index);
             Map(m => m.Learner.GivenNames).Name(@"Given names").Index(++index);
+            Map(m => m.FM25Learner.TLevelStudent).Name(@"TLevelStudent").Index(++index).TypeConverter<IlrBooleanConverter>();
             Map(m => m.Learner.DateOfBirthNullable).Name(@"Date of birth").Index(++index);
             Map(m => m.Learner.CampId).Name(@"Campus identifier").Index(++index);
             Map(m => m.Learner.PlanLearnHoursNullable).Name(@"Planned learning hours").Index(++index);
             Map(m => m.Learner.PlanEEPHoursNullable).Name(@"Planned employability, enrichment and pastoral hours").Index(++index);
             Map(m => m.TotalPlannedHours).Name(@"Total planned hours").Index(++index);
             Map(m => m.FM25Learner.RateBand).Name(@"Funding band").Index(++index);
-            Map(m => m.FM25Learner.StartFund).Name(@"Qualifies for funding").Index(++index);
+            Map(m => m.FM25Learner.StartFund).Name(@"Qualifies for funding").Index(++index).TypeConverter<IlrBooleanConverter>();
             Map(m => m.FM25Learner.OnProgPayment).Name(@"Total funding").Index(++index);
             Map().Name(@"OFFICIAL-SENSITIVE").Constant(string.Empty).Index(++index);
         }
