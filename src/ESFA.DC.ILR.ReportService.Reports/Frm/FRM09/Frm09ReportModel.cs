28c946f9ee7d2e36205955aeab3680079d3c206c
kelvin.morris <kelvin.morris@eas.co.uk>
2020-09-01 15:18:16 +0100

    #104249 Updating model

--- src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM09/Frm09ReportModel.cs
+++ src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM09/Frm09ReportModel.cs
@@ -4,15 +4,24 @@ namespace ESFA.DC.ILR.ReportService.Reports.Frm.FRM09
 {
     public class Frm09ReportModel : FrmBaseReportModel
     {
-        public string PMOrgName;
-        public int? DAUKPRN;
-        public string DAOrgName;
-        public string SOFCode;
+        public string PMOrgName { get; set; }
+
+        public int? DAUKPRN { get; set; }
+
+        public string DAOrgName { get; set; }
+
+        public string SOFCode { get; set; }
+
         public string PrevOrgName { get; set; }
+
         public int AimTypeCode { get; set; }
+
         public DateTime? OrigLearnStartDate { get; set; }
+
         public int? WithdrawalCode { get; set; }
+
         public int FundModel { get; set; }
+
         public string LearnAimType { get; set; }
     }
 }
