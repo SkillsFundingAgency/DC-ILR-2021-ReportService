01becf97c7e50a5aecf1b88b5e827c2b0cc68ae0
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-09-16 08:32:01 +0000

    removed where

--- src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM06/Frm06ReportModelBuilder.cs
+++ src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM06/Frm06ReportModelBuilder.cs
@@ -102,7 +102,6 @@ namespace ESFA.DC.ILR.ReportService.Reports.Frm.FRM06
         {
             HashSet<FrmLearnerKey> learnerKeys = new HashSet<FrmLearnerKey>(_frmEqualityComparer);
             learnerKeys.UnionWith(message.Learners?
-               .Where(l => string.IsNullOrEmpty(l.PrevLearnRefNumber))
                .SelectMany(l => l?.LearningDeliveries
                .Select(ld => new FrmLearnerKey
                {
