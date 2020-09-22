c11434d2bb8071a5c6ce399f7318b03fb0520eb8
Ashley Beecham <ash@momentoussolutions.co.uk>
2020-09-22 16:19:35 +0100

    #105120 - Added filter for learning delivery FundModel/ADL combinations to FRM06 report. fixed other issues found along the way

--- src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM06/Frm06ReportModel.cs
+++ src/ESFA.DC.ILR.ReportService.Reports/Frm/FRM06/Frm06ReportModel.cs
@@ -4,12 +4,12 @@ namespace ESFA.DC.ILR.ReportService.Reports.Frm.FRM06
 {
     public class Frm06ReportModel : FrmBaseReportModel
     {
-        public string PrevOrgName { get; internal set; }
-        public string PMOrgName { get; internal set; }
-        public int AimTypeCode { get; internal set; }
-        public string LearningAimType { get; internal set; }
-        public int FundingModel { get; internal set; }
-        public DateTime? OrigLearnStartDate { get; internal set; }
-        public string SOFCode { get; internal set; }
+        public string PrevOrgName { get; set; }
+        public string PMOrgName { get; set; }
+        public int AimTypeCode { get; set; }
+        public string LearningAimType { get; set; }
+        public int FundingModel { get; set; }
+        public DateTime? OrigLearnStartDate { get; set; }
+        public string SOFCode { get; set; }
     }
 }
